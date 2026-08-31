7f787c745b57973b0d0f0290358c5ef8cd0fe65e
Aman Yadav <yadavaman6940@gmail.com>
2026-08-31 19:24:55 +0530

    Updated CORS to allow Vercel origins

--- src/main/java/com/e_mart/Configuration/SecurityConfig.java
+++ src/main/java/com/e_mart/Configuration/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"));
 
         // 🔑 Important headers
